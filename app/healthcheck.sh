84c937c6054ec4fd3255684c216abe4f5fd540cd
Remo Gloor <remo.gloor@gmail.com>
2021-02-22 00:57:47 +0100

    Update healthcheck.sh

--- app/healthcheck.sh
+++ app/healthcheck.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-exit $(( (`date +%s` - `stat -L -c %Y /log/powerstation-mqtt-status${powerstation_instance}.log` ) > 60 ))
+exit $(( (`date +%s` - `stat -L -c %Y /log/powerstation-mqtt-status-${powerstation_instance}.log` ) > 60 ))
